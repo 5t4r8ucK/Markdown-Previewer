6fa24b1feab35b5d97534e74d24e451d2eaf0faf
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-06-09 22:13:48 +0200

    Initial structure of the app.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './assets/css/App.css';
+
+function Editor(props) {
+  return (
+    <section id="editor">
+      <header>
+        <p>Editor</p>
+      </header>
+      <menu>
+        <li>
+          <button>
+          <i className="fas fa-heading"></i>1
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-heading"></i>2
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-heading"></i>3
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-heading"></i>4
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-heading"></i>5
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-heading"></i>6
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-link"></i>
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-code"></i>
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-th-large"></i>
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-list"></i>
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-quote-left"></i>
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="far fa-image"></i>
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fas fa-bold"></i>
+          </button>
+        </li>
+      </menu>
+      <form>
+        <textarea></textarea>
+      </form>
+    </section>
+  )
+}
+
+function Preview(props) {
+  return (
+    <section id="preview">
+      <header>
+        <p>Preview</p>
+      </header>
+      <article>
+
+      </article>
+    </section>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Editor />
+      <Preview />
+    </React.Fragment>
   );
 }
 
